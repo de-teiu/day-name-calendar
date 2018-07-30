fb046bec381f6c1c9a0ab39b05f3c7c7e5784184
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2018-07-30 22:34:38 +0900

    多分完成

---
+++ assets/js/anniversary.js
@@ -0,0 +1,27 @@
+var SUNDAY = 0;
+var MONDAY = 1;
+var TUESDAY = 2;
+var WEDNESDAY = 3;
+var THURSDAY = 4;
+var FRIDAY = 5;
+var SATURDAY = 6;
+
+var dayNameList = {
+    0: ["Comfortable", "Holy", "Important", "Precious", "Additional"],
+    1: ["Evolutional", "Dynamic", "Brave", "Secure", "Delicious"],
+    2: ["Useful", "Splendid", "Smart", "Emotional", "Gentle"],
+    3: ["Innocent", "Bright", "Beautiful", "Interesting", "Merciful"],
+    4: ["Friendly", "Cheerful", "Delightful", "Dazzling", "Modest"],
+    5: ["Exciting", "Stable", "Healthful", "Quickly", "Elegant"],
+    6: ["Marvelous", "Excellent", "Wonderful", "Fantastic", "Tremendous"]
+};
+
+var dateName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
